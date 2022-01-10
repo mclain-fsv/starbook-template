45a54759b4acec66f42efe6e847023afc8a03452
Mike <49076262+mclain-fsv@users.noreply.github.com>
2022-01-10 15:24:53 -0600

    Create app structure (#1)
    * init CRA commit
    
    * initial header and style changes
    
    * initial header and style changes

---
+++ starbook/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="AppBanner">
+        <span className="TextLogo">Starbook</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
