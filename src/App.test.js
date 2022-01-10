c8c65919cfe5d8f4f193310501ac9f3a4d45289c
mmclain <michael.mclain@fiserv.com>
2022-01-10 15:57:25 -0600

    fix unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Starbook text logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logo = screen.getByText(/Starbook/i);
+  expect(logo).toBeInTheDocument();
 });
