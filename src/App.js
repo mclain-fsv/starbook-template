de6f25a4e6d76562b01ea99205a916031d5c8a98
mmclain <michael.mclain@fiserv.com>
2022-03-22 08:21:08 -0500

    starting template. new icons and manifest.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,41 @@ const App = () => {
       <header className="AppBanner">
         <span className="TextLogo">Starbook</span>
       </header>
+      <main className="UseGutters">
+        <h1>Home</h1>
+        <section>
+          <h2>To do:</h2>
+            <ol>
+              <li>Make a GET request to this endpoint: <code>https://swapi.dev/api/people/1/</code></li>
+              <li>Create a card component with the JSON data returned from the endpoint. The card should display:</li>
+              <ul>
+                <li>Name</li>
+                <li>Birthday</li>
+                <li>Homeworld</li>
+              </ul>
+              <li>Create three clickable entities which will toggle a new card on the page that displays additional information about the person:</li>
+              <ul>
+                <li>Films</li>
+                  <ul>
+                    <li>Film #</li>
+                    <li>Title</li>
+                    <li>Director</li>
+                    <li>Release Date</li>
+                  </ul>
+                <li>Starships</li>
+                  <ul>
+                    <li>Name</li>
+                    <li>Starship Class</li>
+                  </ul>
+                <li>Vehicles</li>
+                  <ul>
+                    <li>Name</li>
+                    <li>Manufacturer</li>
+                  </ul>
+              </ul>
+            </ol>
+        </section>
+      </main>
     </div>
   );
 }
